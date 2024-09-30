abb2029d58b58776900c86ddb896bd3b61fe90ed
Ashirvad <109420822+ashirvadsingh000@users.noreply.github.com>
2024-09-30 20:24:32 +0530

    change main .js
    or node

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Header from './components/Header.jsx'
 import './index.css'
-import Rootp from './Rootp.jsx'
+import Rootp from '../src/Rootp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
